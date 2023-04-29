5dcefbd1c758967ad97895df1e903f7e1ba9391f
zzj <912290893@qq.com>
2023-04-29 18:53:25 +0800

    001

---
+++ machine/001_MaximizeControlOfResourceCosts/demo/javaDemo/src/Main.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static int max_machine;
+
+    public static void main(String[] args) {
+        //处理输入
+        Scanner in = new Scanner(System.in);
+        int taskNum = in.nextInt();
+
+        //构造输入数据结构
+        ArrayList<ArrayList<Integer>> outerArrayList = new ArrayList<ArrayList<Integer>>();
+
+        for (int i = 0; i < taskNum; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int c = in.nextInt();
+            ArrayList<Integer> innerArraylist = new ArrayList<Integer>();
+            innerArraylist.add(a);
+            innerArraylist.add(b);
+            innerArraylist.add(c);
+            outerArrayList.add(innerArraylist);
+            //避免完全没有交集的特殊情况
+            if (max_machine < c) {
+                max_machine = c;
+            }
+        }
+
+        //递归求交集
+        while (outerArrayList.size() > 1) {
+            outerArrayList = cal_public_range(outerArrayList);
+        }
+        System.out.println(max_machine);
+    }
+
+    public static ArrayList<ArrayList<Integer>> cal_public_range(ArrayList<ArrayList<Integer>> ranges) {
+        //先排序
+        Comparator<ArrayList<Integer>> myComparator = new Comparator<ArrayList<Integer>>() {
+            @Override
+            public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
+                if (o1.get(0) != o2.get(0)) {
+                    return o1.get(0) - o2.get(0);
+                } else {
+                    return o1.get(1) - o2.get(2);
+                }
+            }
+        };
+        Collections.sort(ranges, myComparator);
+
+        //求交集
+        ArrayList<ArrayList<Integer>> public_range = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < ranges.size(); i++) {
+            for (int j = i + 1; j < ranges.size(); j++) {
+                int left = Math.max(ranges.get(i).get(0), ranges.get(j).get(0));
+                int right = Math.min(ranges.get(i).get(1), ranges.get(j).get(1));
+                if (left <= right) {
+                    ArrayList<Integer> temp = new ArrayList<Integer>();
+                    temp.add(left);
+                    temp.add(right);
+                    temp.add(ranges.get(i).get(2) + ranges.get(j).get(2));
+                    public_range.add(temp);
+                    if (ranges.get(i).get(2) + ranges.get(j).get(2) > max_machine) {
+                        max_machine = ranges.get(i).get(2) + ranges.get(j).get(2);
+                    }
+                }
+            }
+        }
+        return public_range;
+    }
+}
--- machine/001_MaximizeControlOfResourceCosts/main.cpp
+++ machine/001_MaximizeControlOfResourceCosts/main.cpp
@@ -2,79 +2,43 @@
 #include <vector>
 #include <tuple>
 #include <algorithm>
+
 using namespace std;
 
-bool compare(tuple<int,int,int> a, tuple<int,int,int> b){
-    if(get<0>(a) < get<0>(b)){
+bool compare(tuple<int, int, int> a, tuple<int, int, int> b) {
+    if (get<0>(a) < get<0>(b)) {
         return true;
-    } else if(get<0>(a) == get<0>(b)){
-        if(get<1>(a) < get<1>(b)){
+    } else if (get<0>(a) == get<0>(b)) {
+        if (get<1>(a) < get<1>(b)) {
             return true;
-        } else{
+        } else {
             return false;
         }
-    } else{
+    } else {
         return false;
     }
 }
 
 int main() {
-    vector<tuple<int,int,int>> vecTuple;
+    vector<tuple<int, int, int>> vecTuple;
     int taskNum{0};
-    cin>>taskNum;
+    cin >> taskNum;
     for (int i = 0; i < taskNum; ++i) {
         int startTime;
         int endTime;
         int paralleism;
 
         cin >> startTime >> endTime >> paralleism;
-        tuple<int,int,int> tmp =  make_tuple(startTime, endTime, paralleism);
+        tuple<int, int, int> tmp = make_tuple(startTime, endTime, paralleism);
         vecTuple.emplace_back(tmp);
     }
 
     sort(vecTuple.begin(), vecTuple.end(), compare);
 
-    for(auto ele : vecTuple){
+    for (auto ele: vecTuple) {
         cout << get<0>(ele) << "  " << get<1>(ele) << "  " << get<2>(ele) << endl;
     }
 
 
-
     return 0;
 }
-
-
-//public static ArrayList<ArrayList<Integer>> cal_public_range(ArrayList<ArrayList<Integer>> ranges) {
-//    //先排序
-//    Comparator<ArrayList<Integer>> myComparator = new Comparator<ArrayList<Integer>>() {
-//        @Override
-//        public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
-//            if (o1.get(0)!=o2.get(0)){
-//                return o1.get(0)-o2.get(0);
-//            }else {
-//                return o1.get(1)-o2.get(2);
-//            }
-//        }
-//    };
-//    Collections.sort(ranges, myComparator);
-//
-//    //求交集
-//    ArrayList<ArrayList<Integer>> public_range = new ArrayList<ArrayList<Integer>> ();
-//    for (int i=0;i<ranges.size();i++) {
-//        for (int j=i+1;j<ranges.size();j++) {
-//            int left = Math.max(ranges.get(i).get(0), ranges.get(j).get(0));
-//            int right = Math.min(ranges.get(i).get(1), ranges.get(j).get(1));
-//            if (left <= right) {
-//                ArrayList<Integer> temp = new ArrayList<Integer>();
-//                temp.add(left);
-//                temp.add(right);
-//                temp.add(ranges.get(i).get(2)+ ranges.get(j).get(2));
-//                public_range.add(temp);
-//                if (ranges.get(i).get(2)+ ranges.get(j).get(2) > max_machine) {
-//                    max_machine = ranges.get(i).get(2)+ ranges.get(j).get(2);
-//                }
-//            }
-//        }
-//    }
-//    return public_range;
-//}
